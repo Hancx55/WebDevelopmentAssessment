73430ff58d1d67b6597679e349cfc0c9c7e13d87
Hancx55 <taylorlongworthh@gmail.com>
2026-03-30 19:03:43 +0100

    validation added for payment details

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -50,3 +50,81 @@ if (years) {
         years.appendChild(option);
     }
 }
+
+//validation for the payment details
+
+const validateCardNumber = (num) => {
+    let isValid = false;
+
+    let regex = /^5[1-5]\d{14}$/;
+    if (regex.test(num)) {
+        isValid = true;
+    }
+
+    return isValid;
+}
+
+const validateDate = (month,year) => {
+
+    currdate = new Date();
+    m = currdate.getMonth()+1;
+    y = currdate.getFullYear(); 
+
+    let isValid = false;
+
+    let regexMonth = /^([1-9]|1[0-2])$/;
+    let regexYear = /^(19|20)\d{2}$/;
+
+    if (regexMonth.test(month) && regexYear.test(year)) {
+
+        month = parseInt(month);
+        year = parseInt(year);
+
+        if ((year > y) || (year == y && month >= m)) {
+        isValid = true;
+        }
+    }
+
+    return isValid;
+}
+console.log(validateDate("05","2026"));
+
+const validateCVV = (cvv) => {
+    let isValid = false;
+
+    let regex= /^\d{3,4}$/;
+
+    if (regex.test(cvv)) {
+        isValid = true;
+    }
+
+    return isValid;
+}
+
+// continue button for payment form 
+
+const continueBtn = document.getElementById("continue");
+
+continueBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const cardNumber = document.getElementById("cardNumber").value;
+    const expMonth = document.getElementById("expMonth").value;
+    const expYear = document.getElementById("expYear").value;
+    const cvv = document.getElementById("cvv").value;
+
+    if (validateCardNumber(cardNumber) == false) {
+        console.log("invalid card number");
+        document.getElementById("validation").innerHTML = "Card number is invalid - try again";
+    }
+    else if (validateDate(expMonth, expYear) == false) {
+        console.log("card has expired");
+        document.getElementById("validation").innerHTML = "Card has expired -try again";
+    }
+    else if (validateCVV(cvv) == false) {
+        console.log("invalid security code");
+        document.getElementById("validation").innerHTML = "Invalid security code - try again";
+    }
+    else console.log("payment details valid");
+})
+
+
