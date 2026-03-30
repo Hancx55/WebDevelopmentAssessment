e202b1d2fecbbacb637bbf9d55440abe4e1c474a
Hancx55 <taylorlongworthh@gmail.com>
2026-03-30 16:29:30 +0100

    responsive design implemented well, nav now inserted to all pages

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,9 +1,52 @@
-const navBtn = document.getElementsByClassName('navBtn');
-const sideNav = document.getElementById('sideNav');
+//inserts nav bar for all pages
 
-Array.from(navBtn).forEach((btn) =>
-    btn.addEventListener('click', () => {
-        console.log("clicked");
-        toggleEvent = sideNav.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/Components/header.html")
+    .then(response => response.text())
+    .then(nav => {
+        document.getElementById("navigation").innerHTML = nav;
+        console.log("nav loaded");
+        
+    //side navigation button
+    const navBtn = document.getElementsByClassName('navBtn');
+    const sideNav = document.getElementById('sideNav');
+
+    Array.from(navBtn).forEach((btn) =>
+        btn.addEventListener('click', () => {
+            console.log("clicked");
+            toggleEvent = sideNav.classList.toggle('open');
+        })
+    )
+
     })
-)
+})
+
+
+
+
+
+
+//years and months drop down - payment details
+
+const months = document.getElementById("expMonth");
+
+if (months) {
+    for (let i=1; i<=12; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        months.appendChild(option);
+    }
+}
+
+
+const years = document.getElementById("expYear");
+
+if (years) {
+    for(let i=2020; i<=2035; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        years.appendChild(option);
+    }
+}
